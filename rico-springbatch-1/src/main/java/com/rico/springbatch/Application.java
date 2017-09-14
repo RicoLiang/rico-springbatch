8525366a343fdb7414f2712aba14611a7574df9f
unknown <xianfu.liang@bf.cn>
2017-09-14 18:50:26 +0800

    初始导入

---
+++ rico-springbatch-1/src/main/java/com/rico/springbatch/Application.java
@@ -0,0 +1,22 @@
+package com.rico.springbatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.rico.springbatch")
+public class Application implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		LOGGER.info("rico-springbatch-1已启动");
+	}
+}
